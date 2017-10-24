293c5174c7891fa859dc0857f6e3e754c4b496ad
ryarleque <renzoyarleque@gmail.com>
2017-10-24 10:50:57 -0500

    implement responsive header

--- english-project/src/app/app.module.ts
+++ english-project/src/app/app.module.ts
@@ -17,7 +17,7 @@ export const ROUTES: Routes = [
   declarations: [
     AppComponent,
     BienvenidaComponent,
-    ProgramacionComponent    
+    ProgramacionComponent   
   ],
   imports: [
     BrowserModule,
