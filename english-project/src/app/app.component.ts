d9a37db8727af03342d4d9694f5b91a16ae7b6c2
ryarleque <renzoyarleque@gmail.com>
2017-10-24 15:05:39 -0500

    added navbar part1

--- english-project/src/app/app.component.ts
+++ english-project/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('navBarAnimation', [
+      state('collapsable', style({
+        width: '1rem',
+      })),
+      state('expandable', style({
+        width: '15rem',
+      })),
+      transition('collapsable <=> expandable', animate('300ms ease-in')),
+    ]),
+  ]
 })
+
 export class AppComponent {
   title = 'app';
+
+  state: string = 'collapsable';
+
+  animateMe() {
+    this.state = (this.state =='collapsable' ? 'expandable' : 'collapsable');
+    
+  }
+
 }
--- english-project/src/app/app.module.ts
+++ english-project/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
 import { ProgramacionComponent } from './programacion/programacion.component';
+
+
 
 export const ROUTES: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -21,7 +25,8 @@ export const ROUTES: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES, {useHash: true})
+    RouterModule.forRoot(ROUTES, {useHash: true}),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
